6e48d37ab8a3a90429a531accc827f04bee567cc
gwrgwr <murilomassa72@gmail.com>
2025-05-06 16:38:34 -0300

    feat: add user delete request queue and routing key to RabbitUserConstants

--- src/main/java/com/technova/user/constants/RabbitUserConstants.java
+++ src/main/java/com/technova/user/constants/RabbitUserConstants.java
@@ -7,6 +7,8 @@ public class RabbitUserConstants {
     public static final String USER_SAVE_REQUEST_ROUTING_KEY = "user.save";
     public static final String USER_LOGIN_REQUEST_ROUTING_KEY = "user.login";
     public static final String USER_FIND_BY_ID_REQUEST_ROUTING_KEY = "user.find_by_id";
+    public static final String USER_DELETE_REQUEST_QUEUE = "user-delete-request";
+    public static final String USER_DELETE_REQUEST_ROUTING_KEY = "user.delete";
     public static final String USER_EXCHANGE = "user-exchange";
 
     public RabbitUserConstants() {}
